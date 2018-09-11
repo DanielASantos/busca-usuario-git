069c37d56c40760ec9287145c40cbd8caf97a744
Daniel Andrade dos Santos <daniel.andradesantos88@gmail.com>
2018-09-11 15:57:15 -0300

    Adicionando as funções de controle

---
+++ js/controller.js
@@ -0,0 +1,79 @@
+$("#dados-usuario").hide();
+$("#lista-repos").hide();
+$("#botao-busca").click(buscaUsuario);
+
+//Busca o usuário pelo username do GitHub
+function buscaUsuario(){
+	var usuario = $("#gh-usuario").val();
+
+	$.get("https://api.github.com/users/" + usuario, detalhesUsuario).fail(mensagemErro);
+	$.get("https://api.github.com/users/"+ usuario + "/repos", listagemRepositorios).fail(mensagemErro);
+}
+
+//Apresenta na tela os dados do usuário
+function detalhesUsuario(data){
+	$("#dados-usuario").show();
+	$("#lista-repos").show();
+	$("#erro").hide();
+
+	var avatar = $("#avatar").attr('src', data.avatar_url);
+	var nome = $("#nome");
+	var seguidores = $("#seguidores");
+	var seguidos = $("#seguidos");
+	var email = $("#email");
+	var bio = $("#bio");
+
+	nome.text(data.name);
+	seguidores.text(data.followers);
+	seguidos.text(data.following);
+	email.text(data.email);
+	bio.text(data.bio);	
+}
+
+//Apresenta na tela os repositórios do usuário
+function listagemRepositorios(data){
+	var repositorios = data;
+
+	for (var i = 0, len = data.length; i < len; i++) {
+		console.log(repositorios);
+		var repositoriosLista = $("<a href='#dados-repos' rel='modal:open' id=" + i + "></a><br/>").text(data[i].name);
+		$("#lista-repos").append(repositoriosLista);		   	
+	}
+	//Verifica qual repositório selecionado, e faz a busca dos dados do mesmo
+	$("a").click((e) => {
+       	var selecionado = $(e.target).attr('id');
+    	$.get("https://api.github.com/repos/" + repositorios[selecionado].full_name, detalhesRepositorio)
+	});
+}
+
+//Apresenta em um modal os dados do repositório selecionado
+function detalhesRepositorio(data){
+	console.log("Entrou deta repos");
+	var repositorio = $("#repositorio");
+	var estrela = $("#estrela");
+	var linguagem = $("#linguagem");
+	var descricao = $("#descricao");
+	var link = $("#link-repo").attr('href', data.html_url);
+
+	repositorio.text(data.name);
+	estrela.text(data.stargazers_count);
+	linguagem.text(data.language);
+	descricao.text(data.description);
+	link.text(data.html_url);
+}
+
+//Apresenta mensagem, caso algum erro ocorra na busca
+function mensagemErro () {
+	//$('erro').toggle();
+	$("#dados-usuario").hide();
+	$("#lista-repos").hide();
+	$("#erro").show();
+	//setTimeout(function(){
+	//	$('erro').toggle();
+	//}, 3000);
+}
+
+
+
+
+
